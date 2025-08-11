b18203346efeaa9bd34977e4b552c904ccfed94a
busyarone <busyarone@gmail.com>
2025-08-10 23:17:03 -0700

    Dynamic Array generation in Java

---
+++ java/DynamicArray.java
@@ -0,0 +1,52 @@
+class DynamicArray {
+    int capacity = 1;
+    int size = 0;
+    int[] list;
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        list = new int[capacity];
+    }
+
+    public int get(int i) {
+        int returnValue = (i < size)? list[i]: -1;
+        return returnValue;
+    }
+
+    public void set(int i, int n) {
+        //if(list.length==capacity)
+        //    resize();
+        list[i] = n;
+    }
+
+    public void pushback(int n) {
+        int lastIndex = getSize();
+        if(lastIndex == capacity)
+            resize();
+        list[lastIndex] = n;
+        size+= 1;
+    }
+
+    public int popback() {
+        int lastIndex = getSize()-1;
+        size-= 1;
+        return list[lastIndex];
+    }
+
+    private void resize() {
+        capacity = capacity * 2;
+        int[] newList = new int[capacity];
+        for(int i=0;i<size;i++){
+            newList[i] = list[i];
+        }
+        list = newList;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+}
---
+++ java/DynamicArrayList.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class DynamicArrayList {
+    int capacity = 1;
+    List<Integer> list = new ArrayList<Integer>();
+    public DynamicArrayList(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int get(int i) {
+        int returnValue = (list.size()>=i) ? list.get(i) : -1;
+        return returnValue;
+    }
+
+    public void set(int i, int n) {
+        //check if size is equal to capacity. If so resize the dynamic array
+        if (list.size()==this.capacity)
+            this.resize();
+        list.set(i,n);
+
+    }
+
+    public void pushback(int n) {
+        if (this.list.size()==this.capacity)
+            this.resize();
+        list.add(n);
+    }
+
+    public int popback() {
+        return list.remove(this.list.size()-1);
+    }
+
+    private void resize() {
+        this.capacity = this.capacity*2;
+    }
+
+    public int getSize() {
+        return this.list.size();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public static void main(String[] args) {
+        DynamicArrayList dynamicArray = new DynamicArrayList(2);
+        dynamicArray.pushback(1);
+        dynamicArray.pushback(2);
+        System.out.println("Size: " + dynamicArray.getSize() + ", Capacity: " + dynamicArray.getCapacity());
+        System.out.println("Element at index 0: " + dynamicArray.get(0));
+        System.out.println("Element at index 1: " + dynamicArray.get(1));
+        System.out.println("Popped element: " + dynamicArray.popback());
+        System.out.println("Size after pop: " + dynamicArray.getSize());
+    }
+}
